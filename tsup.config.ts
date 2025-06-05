bd4230086540a745b5d2bcb5eb51f4e798480ea5
Ahmed Gamal <ahmed.gamalhamed5@gmail.com>
2025-06-05 05:23:12 +0300

    fix/ type declaration

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,10 +3,20 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/index.ts'],
   format: ['cjs', 'esm'],
-  dts: true,
+  dts: {
+    resolve: true,
+    entry: ['src/index.ts', 'src/virtual-module.d.ts'],
+  },
   clean: true,
   sourcemap: true,
   minify: false,
   external: ['vite', 'fast-glob'],
   target: 'node16',
+  splitting: false,
+  outExtension({ format }) {
+    return {
+      js: format === 'cjs' ? '.js' : '.mjs',
+      dts: '.d.ts',
+    };
+  },
 });
